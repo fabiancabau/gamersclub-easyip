40b9069ccd892806ff75c4aa995876c9ad2916a9
Fabian Cabau <fabiancabau@gmail.com>
2024-10-05 00:37:19 -0300

    fix

--- content.js
+++ content.js
@@ -8,7 +8,8 @@
   const checkmarkIconSvg = `<svg id='Checkmark_24' width='24' height='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><rect width='24' height='24' stroke='none' fill='#FFFFFF' opacity='0'/><g transform="matrix(1.09 0 0 1.09 12 12)" ><path style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" translate(-12.5, -12.35)" d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688 z" stroke-linecap="round" /></g></svg>`;
 
   setInterval(() => {
-    if (ip != null) {
+
+    if (ip == null) {
       fetch(targetUrl)
         .then((response) => response.json())
         .then((data) => {
